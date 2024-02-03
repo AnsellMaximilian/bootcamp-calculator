5d20efe3bdaa1828d67144887df60b0c18a2e7bf
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-02-03 11:50:06 +0700

    standard calc

--- index.js
+++ index.js
@@ -1,13 +1,112 @@
+"use strict";
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.opMap = exports.Operation = void 0;
+var Operation;
+(function (Operation) {
+    Operation["ADD"] = "ADD";
+    Operation["SUB"] = "SUB";
+    Operation["MUL"] = "MUL";
+    Operation["DIV"] = "DIV";
+})(Operation || (exports.Operation = Operation = {}));
+exports.opMap = (_a = {},
+    _a[Operation.ADD] = "+",
+    _a[Operation.SUB] = "-",
+    _a[Operation.MUL] = "x",
+    _a[Operation.DIV] = "/",
+    _a);
 var numBtns = document.querySelectorAll(".num-btn");
-var display = document.querySelector("#display");
+var opBtns = document.querySelectorAll(".op-btn");
+var evalBtn = document.querySelector("#eval-btn");
+var clearBtn = document.querySelector("#clear-btn");
+var signBtn = document.querySelector("#sign-btn");
+var mainDisplay = document.querySelector("#display__main");
+var subDisplay = document.querySelector("#display__sub");
+var leftVal = 0;
+var rightVal = null;
+var currentOp = null;
+var shouldReset = false;
 numBtns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
         if (e.target) {
             var btn_1 = e.target;
-            display.textContent = btn_1.dataset.value;
+            if (shouldReset) {
+                shouldReset = false;
+                clear();
+            }
+            if (!currentOp) {
+                var newLeftVal = appendNumber(leftVal, parseFloat(btn_1.dataset.value));
+                leftVal = newLeftVal;
+                mainDisplay.textContent = newLeftVal.toString();
+            }
+            else {
+                var newRightVal = appendNumber(rightVal || 0, parseFloat(btn_1.dataset.value));
+                rightVal = newRightVal;
+                mainDisplay.textContent = newRightVal.toString();
+            }
         }
     });
 });
-var getDisplayValue = function () {
-    return parseFloat(display.textContent || "0");
+opBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        var btn = e.target;
+        shouldReset = false;
+        addOp(Operation[btn.dataset.value || "ADD"]);
+    });
+});
+evalBtn.addEventListener("click", function () {
+    evaluate();
+});
+clearBtn.addEventListener("click", function () { return clear(); });
+signBtn.addEventListener("click", function () {
+    var newVal = -leftVal;
+    if (!currentOp) {
+        leftVal = newVal;
+    }
+    else {
+        rightVal = newVal;
+    }
+    mainDisplay.textContent = newVal.toString();
+});
+var appendNumber = function (oldVal, num) {
+    return parseFloat(oldVal.toString() + num.toString());
+};
+var addOp = function (op) {
+    currentOp = op;
+    subDisplay.textContent = leftVal + " " + exports.opMap[op];
+};
+var evaluate = function () {
+    if (rightVal != null && currentOp) {
+        subDisplay.textContent = subDisplay.textContent + " " + rightVal + " =";
+        var res = void 0;
+        switch (currentOp) {
+            case Operation.ADD:
+                res = leftVal + rightVal;
+                break;
+            case Operation.SUB:
+                res = leftVal - rightVal;
+                break;
+            case Operation.MUL:
+                res = leftVal * rightVal;
+                break;
+            case Operation.DIV:
+                res = leftVal / rightVal;
+                break;
+            default:
+                res = 0;
+                break;
+        }
+        leftVal = res;
+        currentOp = null;
+        rightVal = null;
+        mainDisplay.textContent = res.toString();
+        shouldReset = true;
+    }
+};
+var clear = function () {
+    leftVal = 0;
+    currentOp = null;
+    rightVal = null;
+    subDisplay.textContent = "";
+    mainDisplay.textContent = leftVal.toString();
 };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,128 @@
-const numBtns: NodeListOf<HTMLButtonElement> =
-  document.querySelectorAll(".num-btn");
-const display = document.querySelector("#display")!;
+export enum Operation {
+  ADD = "ADD",
+  SUB = "SUB",
+  MUL = "MUL",
+  DIV = "DIV",
+}
+
+export const opMap = {
+  [Operation.ADD]: "+",
+  [Operation.SUB]: "-",
+  [Operation.MUL]: "x",
+  [Operation.DIV]: "/",
+};
+
+const numBtns = document.querySelectorAll(".num-btn");
+const opBtns = document.querySelectorAll(".op-btn");
+const evalBtn = document.querySelector("#eval-btn")!;
+const clearBtn = document.querySelector("#clear-btn")!;
+const signBtn = document.querySelector("#sign-btn")!;
+const mainDisplay = document.querySelector("#display__main")!;
+const subDisplay = document.querySelector("#display__sub")!;
+
+let leftVal = 0;
+let rightVal: number | null = null;
+let currentOp: Operation | null = null;
+
+let shouldReset = false;
 
 numBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     if (e.target) {
       const btn = e.target as HTMLButtonElement;
-      display.textContent = btn.dataset.value as string;
+      if (shouldReset) {
+        shouldReset = false;
+        clear();
+      }
+
+      if (!currentOp) {
+        const newLeftVal = appendNumber(
+          leftVal,
+          parseFloat(btn.dataset.value as string)
+        );
+        leftVal = newLeftVal;
+        mainDisplay.textContent = newLeftVal.toString();
+      } else {
+        const newRightVal = appendNumber(
+          rightVal || 0,
+          parseFloat(btn.dataset.value as string)
+        );
+        rightVal = newRightVal;
+        mainDisplay.textContent = newRightVal.toString();
+      }
     }
   });
 });
 
-const getDisplayValue = () => {
-  return parseFloat(display.textContent || "0");
+opBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const btn = e.target as HTMLButtonElement;
+    shouldReset = false;
+    addOp(Operation[btn.dataset.value || "ADD"]);
+  });
+});
+
+evalBtn.addEventListener("click", () => {
+  evaluate();
+});
+
+clearBtn.addEventListener("click", () => clear());
+
+signBtn.addEventListener("click", () => {
+  const newVal = -leftVal;
+  if (!currentOp) {
+    leftVal = newVal;
+  } else {
+    rightVal = newVal;
+  }
+  mainDisplay.textContent = newVal.toString();
+});
+
+const appendNumber = (oldVal: number, num: number) => {
+  return parseFloat(oldVal.toString() + num.toString());
+};
+
+const addOp = (op: Operation) => {
+  currentOp = op;
+  subDisplay.textContent = leftVal + " " + opMap[op];
+};
+
+const evaluate = () => {
+  if (rightVal != null && currentOp) {
+    subDisplay.textContent = subDisplay.textContent + " " + rightVal + " =";
+
+    let res: number;
+
+    switch (currentOp) {
+      case Operation.ADD:
+        res = leftVal + rightVal;
+        break;
+      case Operation.SUB:
+        res = leftVal - rightVal;
+        break;
+      case Operation.MUL:
+        res = leftVal * rightVal;
+        break;
+      case Operation.DIV:
+        res = leftVal / rightVal;
+        break;
+
+      default:
+        res = 0;
+        break;
+    }
+    leftVal = res;
+    currentOp = null;
+    rightVal = null;
+    mainDisplay.textContent = res.toString();
+    shouldReset = true;
+  }
+};
+
+const clear = () => {
+  leftVal = 0;
+  currentOp = null;
+  rightVal = null;
+  subDisplay.textContent = "";
+  mainDisplay.textContent = leftVal.toString();
 };
